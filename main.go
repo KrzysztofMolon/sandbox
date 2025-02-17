8cb6dfedb9442d6b2cad9acbe25cc5f4fec46091
KrzysztofMolon <krzysztof.molon@arista.com>
2025-02-17 10:44:13 +0100

    Add error handling
    User-announcement: [ADDED] Add error handling for fmt.Println()

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	charsPrinted, _ := fmt.Println("Hello user!")
+	charsPrinted, err := fmt.Println("Hello user!")
 	fmt.Println("Printed:", charsPrinted)
+	if err == nil {
+		fmt.Println("Error:", err)
+	}
 }
